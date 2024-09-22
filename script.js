def295802126a5539ba382bc3a80c1c48413d604
Saishree <mailforsaishreesahoo@gmail.com>
2024-09-22 17:33:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//Declare variables
+let attempts = 0;
+let randomNum = Math.floor(Math.random() * 100 + 1);
+
+const guess = document.getElementById("guess");
+const submit = document.getElementById("submit");
+const hint = document.getElementById("hint");
+const attemptsText = document.getElementById("attempts");
+
+
+//Event listeners
+submit.addEventListener("click",checkGuess);
+function checkGuess() {
+    const userValue = Number(guess.value);
+    attempts++;
+
+
+//conditionals
+if (userValue === randomNum) {
+    hint.textContent = "congratulation , you guessed it !";
+} else if (userValue < randomNum) {
+    hint.textContent = "Too low ! Try again .";
+} else {
+    hint.textContent = "Too high ! Try again .";
+}
+attemptsText.textContent = "Attempts :"+ attempts;
+}
+
+
+
+
